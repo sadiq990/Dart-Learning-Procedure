df9546486c17e37e0b9c7b8c5e8cca89f4d2df51
sadiq990 <48443433+sadiq990@users.noreply.github.com>
2022-12-10 00:47:53 +0400

    first

---
+++ test.dart
@@ -0,0 +1,33 @@
+
+void main () {
+  
+  // Stadium information
+  
+  const String stadion = ("Tofiq Behramov");
+  final int stadionSay = 100;
+  
+  // Guest_01 information
+  
+  int guest01Numbers = 20;
+  
+  // Guest_02 information
+  
+  int guest02Numbers = 50;
+  
+  // Calculation step
+  
+  int totalGuestNUmbers = guest01Numbers + guest02Numbers;
+  
+  // Bos yer sayi
+  
+  int boshYersayi = stadionSay - totalGuestNUmbers;
+  
+  // Final Prints:
+  
+  print("Oyunda $totalGuestNUmbers nefer istirak edecek");
+  print("Stadiumda $boshYersayi neferlik yer qalib ");
+  
+  
+  
+  
+}
